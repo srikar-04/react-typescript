56171df2a31eb756a97f94abb7d9f7006831d131
srikar-04 <srikar.code01@gmail.com>
2025-08-12 20:30:03 +0530

    random password generator

--- src/pages/PasswordGenerator.tsx
+++ src/pages/PasswordGenerator.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function PasswordGenerator() {
+
+
+    const [length, setLenght] = useState(8)
+    const [isSymbolAllowed, setIsSymbolAllowed] = useState(false)
+    const [isNumAllowed, setIsNumAllowed] = useState(false)
+    const [randomPassword, setRandomPassword] = useState<string | null>('')
+
+    function randomPasswordGenerator() {
+        let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        let numbers = '0123456789'
+        let specialSymbols = '!@#$%^&*(){}\|?:";'
+
+        let finalString = chars
+        let password = ''
+
+        if (isSymbolAllowed) finalString+=specialSymbols
+        if (isNumAllowed) finalString+=numbers
+
+        for (let index = 0; index < length; index++) {
+            let randomIndex = Math.floor(Math.random() * finalString.length)
+            password+=finalString[randomIndex]
+        }
+
+        setRandomPassword(() => password)
+    }
+
+    useEffect(() => {
+        randomPasswordGenerator()
+    }, [length, isSymbolAllowed, isNumAllowed])
+    
+
   return (
-    <div className='bg-zinc-800 text-white w-full p-4 flex items-center justify-center'>
-        PasswordGenerator
+    <div className='bg-zinc-800 w-full p-4 flex items-center justify-center flex-col'>
+        <h1 className='font-bold text-3xl mb-2 text-white'>Password Generator</h1>
+        <div className='mb-3 px-4 py-3 flex gap-3 w-1/2 justify-center'>
+            <input className='bg-white rounded-xl px-3 py-2' type="text" value={randomPassword ?? 'random password'} onChange={e => setRandomPassword(e.target.value)}/>
+            <button className='bg-blue-400 p-2 rounded-lg text-white cursor-pointer'>Copy</button>
+        </div>
+
+        <div className='w-1/2 flex items-center justify-evenly text-white'>
+            <div>
+                <span className='mr-2'>Length {length}</span>
+                <input type="range" value={length} onChange={(e) => setLenght(Number(e.target.value))}/>
+            </div>
+            <div>
+                <span className='mr-2'>Numbers</span>
+                <input type="checkbox" checked={isNumAllowed} onChange={() => setIsNumAllowed(prev => !prev)}/>
+            </div>
+            <div>
+                <span className='mr-2'>Symbols</span>
+                <input type="checkbox" checked={isSymbolAllowed} onChange={() => setIsSymbolAllowed(prev => !prev)}/>
+            </div>
+        </div>
     </div>
   )
 }
