9d89843e2ab8e87c5762a25f5585cc1a7d3687f2
srikar-04 <srikar.code01@gmail.com>
2025-08-17 21:34:42 +0530

    obtained info from context

--- src/components/context-components/Profile.tsx
+++ src/components/context-components/Profile.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useContext } from "react"
+import UserContext from "../../Context/UserContext"
+
 
 function Profile() {
+
+  const context = useContext(UserContext)
+
+  if(!context) throw new Error('context is null, Profile is not wrapped inside provider')
+
+  const {user} = context
+
   return (
-    <div>Profile</div>
+    <div className="p-8 flex flex-col items-center justify-center gap-6 bg-zinc-700 text-white">
+      <h1 className="font-semibold text-xl">{`username: ${ user ? user.username : 'usename to be entered'}`}</h1>
+      <h1 className="font-semibold text-xl">{`password: ${user ? user.password : 'password to be entered'}`}</h1>
+    </div>
   )
 }
 
